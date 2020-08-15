f8824f409f8193d575a7b298b8b9441fbd2b0348
RinaYoo <62081885+RinaYoo@users.noreply.github.com>
2020-08-16 04:11:54 +1000

    dao for finding diary by date

--- GarudaHacksProject/src/java/garuda/hacks/model/dao/DBDiary.java
+++ GarudaHacksProject/src/java/garuda/hacks/model/dao/DBDiary.java
@@ -5,6 +5,9 @@
  */
 
 package garuda.hacks.model.dao;
+import garuda.hacks.model.Diary;
+import java.sql.*;
+import java.util.*;
 
 /**
  *
@@ -12,4 +15,32 @@ package garuda.hacks.model.dao;
  */
 public class DBDiary {
     
+    private Statement st;
+    private Connection conn;
+    
+    public DBDiary(Connection conn) throws SQLException {
+        st = conn.createStatement();
+    }
+    
+    //find diary from database by date
+    
+    public Diary findDiary(String date) throws SQLException {
+        String fetch = "SELECT * FROM MSS.PRODUCT WHERE DATE='" + date + "'";
+        ResultSet rs = st.executeQuery(fetch);
+        
+        while (rs.next()) {
+            
+            String searchDiaryDate = rs.getString(1);
+            
+            if (searchDiaryDate.equals(date)) {
+                String searchDiaryTitle = rs.getString(2);
+                String searchDiaryProblem = rs.getString(3);
+                String searchDiarySolution = rs.getString(4);
+                
+                return new Diary(searchDiaryDate, searchDiaryTitle, searchDiaryProblem, searchDiarySolution);
+            }
+        }
+        //if not found - return null
+        return null;
+    }
 }
