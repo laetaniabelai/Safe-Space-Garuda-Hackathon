11a1f3d04202d55fb5a6eca1ca932560d03bd28c
RinaYoo <62081885+RinaYoo@users.noreply.github.com>
2020-08-16 03:54:43 +1000

    created diary.java

--- GarudaHacksProject/src/java/garuda/hacks/model/Diary.java
+++ GarudaHacksProject/src/java/garuda/hacks/model/Diary.java
@@ -11,5 +11,49 @@ package garuda.hacks.model;
  * @author campf
  */
 public class Diary {
+    private String date;
+    private String title;
+    private String description;
+    private String solution;
+
+    public Diary(String date, String title, String description, String solution) {
+        this.date = date;
+        this.title = title;
+        this.description = description;
+        this.solution = solution;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSolution() {
+        return solution;
+    }
+
+    public void setSolution(String solution) {
+        this.solution = solution;
+    }
+    
     
 }
